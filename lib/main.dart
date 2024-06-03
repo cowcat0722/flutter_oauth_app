b8657ce4b795c373121efccc97dca6fce0f23dd7
Pakchan <1625787@donga.ac.kr>
2024-06-03 17:08:01 +0900

    카카오 로그인 적용 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:oauthapp/_core/move.dart';
+
 import 'ui/login_page/login_page.dart';
 
 GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // runApp() 호출 전 Flutter SDK 초기화
+  KakaoSdk.init(nativeAppKey: '489477787df6db2d419bd40b7d0499f3');
   runApp(ProviderScope(child: MyApp()));
 }
 
